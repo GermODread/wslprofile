d9f6fdc97385db735998c491d77eaabc11e532f4
GermODread <sebastianpalitang@gmail.com>
2023-02-15 14:56:41 +0100

    added script for ChatGPT cli

---
+++ Skynet.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+
+import os
+import openai
+import sys
+
+api = "API_KEY_HERE"
+
+args = sys.argv[1:]
+
+question = ' '.join(args)
+
+def skynet(q,z):
+  openai.api_key = f"{z}"
+  response = openai.Completion.create(
+    model="text-davinci-003",
+    prompt=f"{question}",
+    temperature=0.9,
+    max_tokens=150,
+    top_p=1,
+    frequency_penalty=0.0,
+    presence_penalty=0.6,
+    echo=True,
+    stop=[" Human:", " AI:"]
+  )
+  print(response.choices[0].text)
+
+def sarcasticmark(q,z):
+  openai.api_key = f"{z}"
+  response = openai.Completion.create(
+    model="text-davinci-003",
+    prompt=f"{question}",
+    temperature=0.5,
+    max_tokens=60,
+    top_p=0.3,
+    frequency_penalty=0.5,
+    presence_penalty=0.0,
+    echo=True
+  )
+  print(response.choices[0].text)
+
+
+if __name__ == "__main__":
+    #sarcasticmarv(question, api)
+    skynet(question, api)
